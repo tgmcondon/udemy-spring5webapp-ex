543829bbd10dedba18bd556b76b47d727afcb1d1
tgmcondon <103388074+tgmcondon@users.noreply.github.com>
2022-04-12 19:23:33 -0500

    Add files via upload

---
+++ src/main/java/eclipse/group001aa/a02spring5webappex/A02Spring5webappExApplication.java
@@ -0,0 +1,13 @@
+package eclipse.group001aa.a02spring5webappex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A02Spring5webappExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A02Spring5webappExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eclipse/group001aa/a02spring5webappex/A02Spring5webappExApplicationTests.java
@@ -0,0 +1,13 @@
+package eclipse.group001aa.a02spring5webappex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A02Spring5webappExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
